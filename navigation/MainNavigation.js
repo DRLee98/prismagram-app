1f1bbd64178320500289909141e8274f99e329a7
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-12-01 22:07:14 +0900

    Tab Navigation Style

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -4,13 +4,22 @@ import { createStackNavigator } from "@react-navigation/stack";
 import TabNavigation from "./TabNavigation";
 import PhotoNavigation from "./PhotoNavigation";
 import MessageNavigation from "./MessageNavigation";
+import { stackStyles } from "./config";
 
 const Stack = createStackNavigator();
 
 export default () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode="none" mode="modal">
+      <Stack.Navigator
+        headerMode="none"
+        mode="modal"
+        screenOptions={{
+          headerStyle: {
+            ...stackStyles,
+          },
+        }}
+      >
         <Stack.Screen name="TabNavigation" component={TabNavigation} />
         <Stack.Screen name="PhotoNavigation" component={PhotoNavigation} />
         <Stack.Screen name="MessageNavigation" component={MessageNavigation} />
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Platform, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
@@ -7,6 +7,8 @@ import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Profile from "../screens/Profile";
 import MessagesLink from "../components/MessagesLink";
+import NavIcon from "../components/NavIcon";
+import { stackStyles } from "./config";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,22 +16,44 @@ const Stack = createStackNavigator();
 
 const stackFactory = (name, initialRoute, customConfig) => (
   <Stack.Navigator>
-    <Stack.Screen name={name} component={initialRoute} options={{ ...customConfig }} />
+    <Stack.Screen name={name} component={initialRoute} options={{ ...customConfig, headerStyle: { ...stackStyles } }} />
   </Stack.Navigator>
 );
 
 export default () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home">
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: "#FAFAFA",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NavIcon focused={focused} name={Platform.OS === "ios" ? "ios-home" : "md-home"} />
+          ),
+        }}
+      >
         {() =>
           stackFactory("Home", Home, {
             title: "Home",
             headerRight: () => <MessagesLink />,
+            headerTitle: () => <Image src={require("../assets/logo.png")} />,
           })
         }
       </Tab.Screen>
-      <Tab.Screen name="Search">
+      <Tab.Screen
+        name="Search"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NavIcon focused={focused} name={Platform.OS === "ios" ? "ios-search" : "md-search"} />
+          ),
+        }}
+      >
         {() =>
           stackFactory("Search", Search, {
             title: "Search",
@@ -45,15 +69,45 @@ export default () => {
             navigation.navigate("PhotoNavigation");
           },
         })}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NavIcon focused={focused} name={Platform.OS === "ios" ? "ios-add" : "md-add"} />
+          ),
+        }}
       />
-      <Tab.Screen name="Notifications">
+      <Tab.Screen
+        name="Notifications"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NavIcon
+              focused={focused}
+              name={
+                Platform.OS === "ios"
+                  ? focused
+                    ? "ios-heart"
+                    : "ios-heart-empty"
+                  : focused
+                  ? "md-heart"
+                  : "md-heart-empty"
+              }
+            />
+          ),
+        }}
+      >
         {() =>
           stackFactory("Notifications", Notifications, {
             title: "Notifications",
           })
         }
       </Tab.Screen>
-      <Tab.Screen name="Profile">
+      <Tab.Screen
+        name="Profile"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NavIcon focused={focused} name={Platform.OS === "ios" ? "ios-person" : "md-person"} />
+          ),
+        }}
+      >
         {() =>
           stackFactory("Profile", Profile, {
             title: "Profile",
